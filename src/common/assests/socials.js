4a188339a7b98c56bcde5f771a0e09d5f29dc123
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2024-02-08 13:02:43 +0100

    added skills and socials content

---
+++ src/common/assests/socials.js
@@ -0,0 +1,28 @@
+import { IconStyle } from '../Footer/styled';
+import { ReactComponent as GithubIcon } from "../images/icons/socialsIcons/gitHub.svg";
+import { ReactComponent as FbIcon } from '../images/icons/socialsIcons/fb.svg';
+import { ReactComponent as LinkedinIcon } from '../images/icons/socialsIcons/linkd.svg';
+import { ReactComponent as InstaIcon } from '../images/icons/socialsIcons/instg.svg';
+
+export const socials = [
+   {
+      name: 'GitHub',
+      url: 'https://github.com/AndriiStafiniak',
+      Icon: IconStyle(GithubIcon),
+   },
+   {
+      name: 'Facebook',
+      url: 'https://www.facebook.com/andrii.stafiniak',
+      Icon: IconStyle(FbIcon),
+   },
+   {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/andrii-stafiniak/',
+      Icon: IconStyle(LinkedinIcon),
+   },
+   {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/staff_andre/',
+      Icon: IconStyle(InstaIcon),
+   },
+]
