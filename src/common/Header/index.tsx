325be611d52b1182c6fd5329337508bcab148356
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2024-11-10 19:48:31 +0100

    updated my portfolio description

--- src/common/Header/index.tsx
+++ src/common/Header/index.tsx
@@ -17,10 +17,11 @@ export const Header = () => {
         <Paragraph>This is</Paragraph>
         <Heading>Andrii Stafiniak</Heading>
         <Description>
-          👨🏻‍💻 I’m a passionate Frontend Developer in love with React. I am
-          looking for my first job and programming challenges. I want to explore
-          new technologies, improve my skills and gain knowledge among other
-          programmers.
+          👨🏻‍💻 I’m an experienced Frontend Developer with a strong focus on React
+          and a background in corporate environments. I’m eager to tackle new
+          programming challenges, explore emerging technologies, and continue
+          expanding my skill set while collaborating with other professionals in
+          the field.
         </Description>
         <Button href="mailto:andrii.stafiniak@gmail.com">
           <HireMe />
